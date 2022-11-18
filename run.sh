49e11dff203b2dacd734b4fef7cffcc5cd44c997
graur <ancongr@yandex.ru>
2022-11-18 14:52:38 +0300

    updated configuration

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+ls -al
 chmod -R 777 test
 docker pull yegor256/c2eo:0.1.24
-docker run -v $(pwd):/eo yegor256/c2eo:0.1.24 /test/hello.c hello.eo
+docker run -v $(pwd):/eo yegor256/c2eo:0.1.24 test/hello.c test/hello.eo
 cat hello.eo
+ls -al
 ls -al test

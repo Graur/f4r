25eeb67a9dc0551261748191efb7080c27273bad
graur <ancongr@yandex.ru>
2022-11-18 15:57:38 +0300

    updated configuration

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ mkdir out
 chmod -R 777 test
 docker pull yegor256/c2eo:0.1.24
 docker run -v $(pwd):/eo yegor256/c2eo:0.1.24 test/hello.c out/hello.eo
-cat hello.eo
+cat out/hello.eo
 ls -al
 ls -al test
 ls -al out

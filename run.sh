c5186af18f1e2069a01a9f43de651aadac3d3237
graur <ancongr@yandex.ru>
2022-11-18 15:37:06 +0300

    updated configuration

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-ls -al
+mkdir out
 chmod -R 777 test
 docker pull yegor256/c2eo:0.1.24
-docker run -v $(pwd):/eo yegor256/c2eo:0.1.24 test/hello.c test/hello.eo
+docker run -v $(pwd):/eo yegor256/c2eo:0.1.24 test/hello.c out/hello.eo
 cat hello.eo
 ls -al
 ls -al test
+ls -al out
+
+#curl -O http://search.maven.org/remotecontent?filepath=org/polystat/polystat/0.6.0/polystat-0.6.0-jar-with-dependencies.jar
+#java -jar target/polystat-0.6.0-jar-with-dependencies.jar --files=test

817e72d6199c24401913bc560f69ffe900229058
graur <ancongr@yandex.ru>
2022-11-22 14:50:28 +0300

    added GOTO elimination

--- dejump.sh
+++ dejump.sh
@@ -6,4 +6,4 @@ echo "Dejump jar was downloaded"
 java -jar dejump-0.0.2-jar-with-dependencies.jar --eo out/global.eo
 echo "GOTO was removed, check the result: "
 ls -la out/generated
-cat out/generated/global.eo
+cat out/generated/global_transformed.eo

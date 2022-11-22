cc6e48d23ab1f1dcd82043f75f6c6b3555ab573e
graur <ancongr@yandex.ru>
2022-11-22 14:26:32 +0300

    added GOTO elimination

---
+++ dejump.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download Dejump jar file
+curl -L -o dejump-0.0.2-jar-with-dependencies.jar "https://repo1.maven.org/maven2/org/eolang/dejump/0.0.2/dejump-0.0.2-jar-with-dependencies.jar"
+echo "Dejump jar was downloaded"
+java -jar dejump-0.0.2-jar-with-dependencies.jar --eo out/global.eo
+echo "GOTO was removed, check the result: "
+cat generated/global.eo
--- polystat.sh
+++ polystat.sh
@@ -4,5 +4,5 @@
 curl -L -o polystat.jar "https://github.com/polystat/polystat-cli/releases/download/v0.1.11/polystat.jar"
 echo "Polystat (EO) analysis has started"
 touch polystat-eo-out.txt
-java -jar polystat.jar eo --in out --to file=polystat-eo-out.txt --sarif
+java -jar polystat.jar eo --in generated --to file=polystat-eo-out.txt --sarif
 echo "Polystat (EO) analysis has finished"o polystat.jar "https://github.com/polystat/polystat-cli/releases/download/v0.1.11/polystat.jar"
